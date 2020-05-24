2f1b2af681105ec55833ff199d39997971587051
FFwt <1029491196@qq.com>
2020-05-24 17:26:11 +0800

    软件测试TestNG

---
+++ src/test/java/com/test/one/Login.java
@@ -0,0 +1,15 @@
+package com.test.one;
+
+public class Login {
+
+	public boolean logintest(String username, String password) {
+		boolean flag = false;
+		if (username == "test" && password == "test") {
+			flag = true;
+			return flag;
+		} else {
+			return flag;
+		}
+
+	}
+}
